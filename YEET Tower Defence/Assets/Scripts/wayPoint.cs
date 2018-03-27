7fcb5080dd282bef0e2b9a9e428a4fbd8a586260
Cihan <cihankurt1998@gmail.com>
2018-03-27 09:53:30 +0200

    Milan
    Milans toevoegingen

---
+++ YEET Tower Defence/Assets/Scripts/wayPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wayPoint : MonoBehaviour {
+
+    public static Transform[] wayPoints;
+
+    void Awake()
+    {
+        wayPoints = new Transform[transform.childCount];
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            wayPoints[i] = transform.GetChild(i);
+        }
+    }
+}
